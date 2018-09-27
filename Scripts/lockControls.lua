4a25b4ff7df146eb61bf2090d5904d6f04ac4b9d
Clamb94 <axel@reinemuth.de>
2018-09-27 13:55:26 +0200

    Added Howto

--- Scripts/lockControls.lua
+++ Scripts/lockControls.lua
@@ -1,3 +1,14 @@
+	--[[
+LockControls by Axel Reinemuth
+
+This FlyWithLua script will automatically disable primary flight control inputs when the autopilot is engaged.
+
+How to use:
+1. Install FlyWithLua ( https://forums.x-plane.org/index.php?/files/file/38445-flywithlua-ng-next-generation-edition-for-x-plane-11-win-lin-mac/ )
+2. Place this Scipt in the folder "X-Plane 11\Resources\plugins\FlyWithLua\Scripts"
+3. Done
+]]--
+
 dataref("override_roll", "sim/joystick/yoke_roll_ratio", "writeable")
 dataref("override_pitch", "sim/joystick/yoke_pitch_ratio", "writeable")
 dataref("override_heading", "sim/joystick/yoke_heading_ratio", "writeable")
