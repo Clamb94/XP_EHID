2050082b7de11f37ae4eb1469322851e635c9dec
Clamb94 <axel@reinemuth.de>
2018-09-27 13:53:26 +0200

    Added Howto

--- Scripts/OpenGuards.lua
+++ Scripts/OpenGuards.lua
@@ -1,3 +1,15 @@
+	--[[
+OpenGuards by Axel Reinemuth
+
+This FlyWithLua script will automatically open all Switch covers/guards in the Zibo 737 for X-Plane.
+
+How to use:
+1. Install FlyWithLua ( https://forums.x-plane.org/index.php?/files/file/38445-flywithlua-ng-next-generation-edition-for-x-plane-11-win-lin-mac/ )
+2. Place this Scipt in the folder "X-Plane 11\Resources\plugins\FlyWithLua\Scripts"
+3. Done
+]]--
+	
+	
 	dataref("cov0", "laminar/B738/button_switch/cover_position", "readonly", 0)
 	dataref("cov1", "laminar/B738/button_switch/cover_position", "readonly", 1)
 	dataref("cov2", "laminar/B738/button_switch/cover_position", "readonly", 2)
